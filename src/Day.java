e54ab411da35821f6814e8039cc3e8bbb7a703f5
AceRosario <haroldace12345@gmail.com>
2024-09-24 17:12:58 -0700

    update1

--- src/Day.java
+++ src/Day.java
@@ -1,10 +1,12 @@
 public class Day {
     private int dayValue;
     protected int monthValue;
+    private int yearValue;
 
-    public Day (int day, int month){
+    public Day (int day, int month, int year){
             this.dayValue = day;
             this.monthValue = month;
+            this.yearValue = year;
     }
 
 
@@ -16,8 +18,25 @@ public class Day {
         return monthValue;
     }
 
+    public int getYearValue() {
+        return yearValue;
+    }
+
+    private boolean isLeapYear() {
+        if ((yearValue % 4 == 0 && yearValue % 100 != 0) || (yearValue % 400 == 0)) {
+            return true;
+        }
+        return false;
+    }
+
     public int findDayNum() {
         int dayNum = 0;
+        int leapDay = 28;
+
+        if (isLeapYear()) {
+            leapDay = 29;
+        }
+
 
         switch (monthValue) {
             case 1:
@@ -27,37 +46,37 @@ public class Day {
                 dayNum = 31 + dayValue;
                 break;
             case 3:
-                dayNum = 31 + 28 + dayValue;
+                dayNum = 31 + leapDay + dayValue;
                 break;
             case 4:
-                dayNum = 31 + 28 + 31 + dayValue;
+                dayNum = 31 + leapDay + 31 + dayValue;
                 break;
             case 5:
-                dayNum = 31 + 28 + 31 + 30 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + dayValue;
                 break;
             case 6:
-                dayNum = 31 + 28 + 31 + 30 + 31 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + dayValue;
                 break;
             case 7:
-                dayNum = 31 + 28 + 31 + 30 + 31 + 30 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + 30 + dayValue;
                 break;
             case 8:
-                dayNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + 30 + 31 + dayValue;
                 break;
             case 9:
-                dayNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + 30 + 31 + 31 + dayValue;
                 break;
             case 10:
-                dayNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + 30 + 31 + 31 + 30 + dayValue;
                 break;
             case 11:
-                dayNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + dayValue;
                 break;
             case 12:
-                dayNum = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + dayValue;
+                dayNum = 31 + leapDay + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + dayValue;
                 break;
             default:
-                System.out.println("invalid");
+                System.out.println("Invalid month");
                 break;
         }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+    inputDate();
+    inputDate();
+
+
+    }
+
+    public static void inputDate() {
+        JTextField dayInput = new JTextField();
+        JTextField monthInput = new JTextField();
+        JTextField yearInput = new JTextField();
+
+        Object[] message = {
+                "Day:", dayInput,
+                "Month:", monthInput,
+                "Year:", yearInput
+        };
+
+        int option = JOptionPane.showConfirmDialog
+                (null, message, "Enter the date", JOptionPane.OK_CANCEL_OPTION);
+
+
+        if (option == JOptionPane.OK_OPTION) {
+            int day = Integer.parseInt(dayInput.getText());
+            int month = Integer.parseInt(monthInput.getText());
+            int year = Integer.parseInt(yearInput.getText());
+
+            Day date = new Day(day, month, year);
+
+
+            JOptionPane.showMessageDialog
+                    (null, "The day number is: " + date.findDayNum());
+        }
 
     }
 }
